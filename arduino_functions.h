274c002bc42ef45a03f219581ebc9d827e8ef0d6
Hugo Visser <hugo@littlerobots.nl>
2024-11-26 23:24:23 +0100

    Add ESP32

--- arduino_functions.h
+++ arduino_functions.h
@@ -6,7 +6,7 @@
 #if (defined(__AVR_ATmega32U4__) || defined(TEENSY20) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega1281__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega2561__) || defined(ESP8266)) && !defined(MAX_RECORDINGS)
   #define MAX_RECORDINGS 512   //on the bigger arduino we have enough SRAM
 #endif
-#if defined(ESP8266)
+#if defined(ESP8266) || defined(ESP32)
   #define MAX_RECORDINGS 512  // ESP8266 has far enough ram
 #endif
 #if !defined(MAX_RECORDINGS)
@@ -26,7 +26,7 @@ static inline void hw_delayMicroseconds(uint32_t time_to_wait) {
   //  delayMicroseconds() only works up to 16383 micros
   // https://github.com/pimatic/rfcontroljs/issues/29#issuecomment-85460916
   while(time_to_wait > 16000) {
-    #if defined(ESP8266)
+    #if defined(ESP8266) || defined(ESP32)
       delay(16);
     #else
       delayMicroseconds(16000);
